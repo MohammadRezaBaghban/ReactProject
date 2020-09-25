5e14f685b84e016df46eeeb1b40a5103da263052
MohammadRezaBaghban <mohammad.haghighi@xaas.farm>
2020-09-25 16:11:08 +0200

    Implement Persons Component

---
+++ instructor-example/src/components/Persons/Persons.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import Person from './Person/Person';
+
+const Persons = (props) => props.persons.map((person, index) => {
+    return (
+        <Person
+            click={() => props.clicked(index)}
+            name={person.name}
+            age={person.age}           
+            changed={event => props.changed(event, person.id)}
+        />                              
+    );
+})
+
+   
+
+
+
+export default Persons;
