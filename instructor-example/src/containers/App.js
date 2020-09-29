729474529b150d4ec3c6da415d583e6e30b194b0
MohammadRezaBaghban <mohammad.haghighi@xaas.farm>
2020-09-29 17:13:53 +0200

    Another type of Higher Order Component in function manner.

--- instructor-example/src/containers/App.js
+++ instructor-example/src/containers/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import withClass from  '../hoc/WithClass';
 
 class App extends Component {
   constructor(props) {
@@ -89,7 +89,7 @@ class App extends Component {
     }
 
     return (
-      <div className={classes.App}>
+      <React.Fragment classes={classes.App}>
         <button
           onClick={() => {
             this.setState({ showCockpit: false });
@@ -106,10 +106,10 @@ class App extends Component {
           />
         ) : null}
         {persons}
-      </div>
+      </React.Fragment>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
-export default App;
+export default withClass(App,classes.App);
